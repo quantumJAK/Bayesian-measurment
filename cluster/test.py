8db38cf7f879fb783a8ea436dafb2e835249af0f
JAK <j.a.krzywda@gmail.com>
2024-06-13 08:21:04 +0200

    sadasd

--- cluster/test.py
+++ cluster/test.py
@@ -1,6 +1,14 @@
 import gymnasium as gym
-
+import matplotlib.pyplot as plt
+import numpy as np
 print("dupa")
+
+x = np.random.random(5)
+y = np.random.random(5)
+plt.plot(x, y)
+plt.savefig("dupa.png")
+
+
 
 from stable_baselines3 import PPO
 
